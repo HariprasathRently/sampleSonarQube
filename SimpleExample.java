ce446cf63ed1d46b7368b62cb7d21d8e5224098c
Hariprasath_HP <79829145+HariprasathRently@users.noreply.github.com>
2025-04-08 15:03:09 +0530

    Add files via upload

---
+++ SimpleExample.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.List;
+
+public class SimpleExample {
+
+    public static void main(String[] args) {
+        // Example with a potential null pointer exception
+        String text = null;
+        System.out.println(text.length()); // This will cause a NullPointerException
+        
+        // Example of an unused variable (SonarQube will detect this)
+        int unusedVariable = 100;
+
+        // Example of a method with too many parameters (SonarQube will flag this)
+        printMessage("Hello, World!", 123, true, 1.5);
+    }
+
+    // Example of a method with too many parameters (SonarQube will flag this)
+    public static void printMessage(String message, int number, boolean flag, double value) {
+        System.out.println(message + " " + number + " " + flag + " " + value);
+    }
+}
