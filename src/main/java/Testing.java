b160a3770a1273a32676fe9c3cd9d42e5f04eb48
Hariprasath_HP <hariprasath.s@rently.com>
2025-05-16 10:46:17 +0530

    fix the syntax

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,5 +1,7 @@
+package com.example.src.main.java;
+
 public class Testing {
-        public int addNumbers(int a int b) {
+        public int addNumbers(int a, int b) {
             int result = a + b;
             return results;
         }
